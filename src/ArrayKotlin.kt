13b054ef714e54ee709925c97b9ae7cf59e3b9bb
aackddlr@gmail.com <xnlrla9395>
2020-02-20 13:53:46 +0900

    array example commit

---
+++ src/ArrayKotlin.kt
@@ -0,0 +1,6 @@
+class ArrayKotlin {
+    var array = arrayOfNulls<Int>(3)
+    var a = arrayOf(1,2,3)
+    val b = Array(5, {i -> (i * i)})
+    var c = intArrayOf(1,2,3)
+}
---
+++ src/ArrayStudy.java
@@ -0,0 +1,8 @@
+import java.lang.reflect.Array;
+
+public class ArrayStudy {
+
+    int[] i_array;
+    int mArray[];
+
+}
